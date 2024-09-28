f589d91e11d1ab751d7e29315df1249c1ee49994
Nour Magdy <neromagdy2@gmail.com>
2024-09-29 00:02:23 +0300

    add social login (google)

--- index.js
+++ index.js
@@ -4,9 +4,54 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 const path = require("path");
-const passport = require('passport');
+
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require("../models/users.model");
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL,
+    passReqToCallback: true,
+    scope: ["profile", "email"]
+}, 
+async function verify (request, accessToken, refreshToken, profile, done){
+    try{
+        // get the returned data from profile
+        let data = profile?._json;
+        let user = await User.findOne({email: data.email})
+
+        if(!user){ // create user, if user does not exist
+            const newUser = await User.create({
+                firstname: data.given_name,
+                lastname: data.family_name,
+                user_image: data.picture,
+                email: data.email
+            });
+            return await done(null, newUser)
+
+        }
+        return await done(null, user)
+
+
+    }catch(error){
+        return done(error, false)
+    }
+}));
+
+passport.serializeUser((user, done)=>{
+    done(null, user)
+})
+
+passport.deserializeUser((user, done) =>{
+    done(null, user);
+});
+
 // const googleStartegy=require('/utils/functions/passportConfig');
 // googleStartegy.passportConfig();
 
